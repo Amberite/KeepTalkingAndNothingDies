f086272d84b69f51c9819edadf3d9411a703a870
Amberite <jakekennard.tj@gmail.com>
2016-11-12 19:30:19 +0200

    Animated Help Window and added buttons

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -3,10 +3,42 @@ using System.Collections;
 
 public class MenuScript : MonoBehaviour {
     public GameObject HelpWindow;
+    public bool helpWindowOpen;
 
-
-    public void HelpButton() {
-        Debug.Log("Hey!");
+    void Start ()
+    {
+        helpWindowOpen = false;
     }
+
+    void Update ()
+    {
+        // if helpwindow is open
+        if (helpWindowOpen == true)
+        {
+            if (Input.anyKeyDown) {
+                // play fade out animation
+                HelpWindow.GetComponent<Animation>().Play("FadeOut");
+                HelpWindow.GetComponent<CanvasGroup>().interactable = false;
+                HelpWindow.GetComponent<CanvasGroup>().blocksRaycasts = false;
+                helpWindowOpen = false;
+
+            }
+        }
+    }
+
+    public void HelpButtonPress() {
+        Debug.Log("Help Button Pressed!");
+        HelpWindow.GetComponent<Animation>().Play("FadeIn");
+        HelpWindow.GetComponent<CanvasGroup>().interactable = true;
+        HelpWindow.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        helpWindowOpen = true;
+    }
+
+    public void StartButtonPress()
+    {
+       Debug.Log("Start Button Pressed! Put Load Level Here in menu script");
+
+    }
+
 
 }
