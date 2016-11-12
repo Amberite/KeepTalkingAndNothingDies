9ede21fc32aee84b0b721e78467e4dec1c74aa3e
Amberite <jakekennard.tj@gmail.com>
2016-11-12 17:27:59 +0200

    Start Menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+    public GameObject HelpWindow;
+
+
+    public void HelpButton() {
+        Debug.Log("Hey!");
+    }
+
+}
